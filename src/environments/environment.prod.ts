b0ea4fe0015a8b8c72cc1050663e487ddc0fc3db
Kemosabe <joshuapearson1993gmail.com>
2021-05-02 23:10:24 +0100

    update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  clientId: "74efdb488b164d48b75c933571d6102a",
+  scopes: "user-read-private user-read-email user-top-read"
 };
